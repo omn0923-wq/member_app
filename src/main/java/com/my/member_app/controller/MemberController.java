39ab2d1b68f8751effefddc72818ce79c165823c
omn0923-wq <omn0923@gmail.com>
2026-05-14 16:05:13 +0900

    feat: Update

--- src/main/java/com/my/member_app/controller/MemberController.java
+++ src/main/java/com/my/member_app/controller/MemberController.java
@@ -56,4 +56,37 @@ public class MemberController {
                 "정상적으로 삭제되었습니다.");
         return "redirect:/member/view";
     }
+
+    @GetMapping("update")
+    public String updateFormView(Model model,
+                                 @RequestParam("updateId") Long updateId,
+                                 RedirectAttributes redirectAttributes) {
+//        1. 선택한 id를 가져오는지 확인
+        log.info("======= updateId: " + updateId);
+//        2. 해당 id를 검색해서 dto 받아온다.
+        MemberDto updateDto = memberService.findById(updateId);
+        log.info("======= updateDto: " + updateDto);
+//        3. updateDto 비어있는지 확인 -> member/view
+        if (updateDto == null) {
+            redirectAttributes.addFlashAttribute("message",
+                    "선택한 데이터가 없습니다.");
+            return "redirect:/member/view";
+        } else {
+            //        4. 모델에 담아서 updateForm에 보낸다.
+            model.addAttribute("dto", updateDto);
+            return "updateMember";
+        }
+    }
+
+    //    update > post
+    @PostMapping("/update")
+    public String update(@ModelAttribute("dto") MemberDto dto,
+                         RedirectAttributes redirectAttributes) {
+//        찍어 보기
+        log.info("uddatedDto: " + dto);
+        memberService.insert(dto);
+        redirectAttributes.addFlashAttribute("message",
+                "정상적으로 수정되었습니다.");
+        return "redirect:/member/view";
+    }
 }
